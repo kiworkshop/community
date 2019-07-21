84b2eee5c09d678eda905d79de4781df520e8506
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2019-07-21 20:22:39 +0900

    [#7] Implement controller.
    - Change the interface of `NoticeService.createNotice()`
    - Add a controller and implement post request handler.

--- mother-api/src/main/java/comunity/mother/notice/service/NoticeService.java
+++ mother-api/src/main/java/comunity/mother/notice/service/NoticeService.java
@@ -1,16 +1,20 @@
 package comunity.mother.notice.service;
 
+import comunity.mother.notice.api.dto.NoticeRequestDto;
 import comunity.mother.notice.domain.Notice;
 import comunity.mother.notice.domain.NoticeRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class NoticeService {
   private final NoticeRepository noticeRepository;
+  private final ModelMapper modelMapper;
 
-  public Notice createNotice(Notice notice) {
-    return noticeRepository.save(notice);
+  public Long createNotice(NoticeRequestDto noticeRequestDto) {
+    Notice notice = modelMapper.map(noticeRequestDto, Notice.class);
+    return noticeRepository.save(notice).getId();
   }
 }
---
+++ mother-api/src/test/java/comunity/mother/notice/api/NoticeControllerTest.java
@@ -0,0 +1,40 @@
+package comunity.mother.notice.api;
+
+import static comunity.mother.notice.api.dto.NoticeRequestDtoTest.getNoticeRequestDtoFixture;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import comunity.mother.notice.api.dto.NoticeRequestDto;
+import comunity.mother.notice.service.NoticeService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest
+class NoticeControllerTest {
+    private @Autowired MockMvc mvc;
+    private @MockBean NoticeService noticeService;
+    private @Autowired ObjectMapper objectMapper;
+
+    @Test
+    void createNotice_ValidInput_ValidOutput() throws Exception {
+        // given
+        NoticeRequestDto noticeRequestDto = getNoticeRequestDtoFixture();
+        given(noticeService.createNotice(any(NoticeRequestDto.class))).willReturn(1L);
+
+        // expect
+        this.mvc.perform(post("/notices")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(noticeRequestDto)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").value(1L));
+    }
+}

c21da93918c3a42853c25f2e12e3c24bd2ce40f9
Myeongjae Kim <dev@myeongjae.kim>
2019-07-31 12:35:15 +0900

    [#14] Add ApiError class and exception controller (#15)
    * [#1] Change package name from comunity to community
    
    * [#1] Add `lombok.config` file to make jacoco ignore lombok annotations
    
    * [#14] Add `ApiError` DTO.
    
    * [#14] Add exception controllers and MyReflectionUtils.
    
    - Add common exception controller.
    - Add mother api exception controller inherited common exception controller.
    - Add `MyReflectionUtils#setField()` for tests.
    
    * [#14] Add build.gradle of community.common package
    
    * [#14] Obey to checkstyle.

---
+++ common/src/test/java/community/common/dto/ApiErrorTest.java
@@ -0,0 +1,19 @@
+package community.common.dto;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+class ApiErrorTest {
+  @Test
+  void constructApiError_ValidInput_ConstructedApiError() {
+    ApiError apiError = new ApiError(HttpStatus.NOT_FOUND, "message");
+
+    then(apiError).hasNoNullFieldsOrProperties();
+    then(apiError.getTimestamp()).isNotNull();
+    then(apiError.getStatus()).isEqualTo(HttpStatus.NOT_FOUND.value());
+    then(apiError.getError()).isEqualTo(HttpStatus.NOT_FOUND.getReasonPhrase());
+    then(apiError.getMessage()).isEqualTo("message");
+  }
+}

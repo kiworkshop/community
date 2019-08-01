2017709cc1105c066ae1c5650d4ba69407ac41f7
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2019-08-01 23:35:26 +0900

    [#20] Obey to checkstyle

--- mother-api/src/test/java/community/mother/notice/api/NoticeControllerTest.java
+++ mother-api/src/test/java/community/mother/notice/api/NoticeControllerTest.java
@@ -1,5 +1,12 @@
 package community.mother.notice.api;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import community.mother.notice.api.dto.NoticeRequestDto;
 import community.mother.notice.api.dto.NoticeRequestDtoTest;
@@ -10,13 +17,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
 class NoticeControllerTest {

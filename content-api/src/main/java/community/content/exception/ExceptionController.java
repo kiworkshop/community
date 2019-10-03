fd98f9d1839b7af493ca2af746ad2d4efcbddaec
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2019-10-03 14:25:02 +0900

    [#37] Pass all tests

--- content-api/src/main/java/community/content/exception/ExceptionController.java
+++ content-api/src/main/java/community/content/exception/ExceptionController.java
@@ -3,6 +3,7 @@ package community.content.exception;
 import community.common.dto.ApiError;
 import community.common.exception.CommonExceptionController;
 import community.content.board_myeongjae.exception.MjArticleNotFoundException;
+import community.content.myanglog.exception.PostNotFoundException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,7 +14,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class ExceptionController extends CommonExceptionController {
 
   @Override
-  @ExceptionHandler(MjArticleNotFoundException.class)
+  @ExceptionHandler(value = {
+      MjArticleNotFoundException.class,
+      PostNotFoundException.class
+  })
   public ApiError handleNotFound(RuntimeException e) {
     return super.handleNotFound(e);
   }
--- content-api/src/test/java/community/content/board_myeongjae/api/MjArticleControllerTest.java
+++ content-api/src/test/java/community/content/board_myeongjae/api/MjArticleControllerTest.java
@@ -31,7 +31,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest
+@WebMvcTest(MjArticleController.class)
 class MjArticleControllerTest {
   private @Autowired
   MockMvc mvc;

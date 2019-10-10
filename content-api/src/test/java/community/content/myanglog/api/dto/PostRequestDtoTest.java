efbc0ce892a0a629e17d7d97d07ac4427f25172a
Myeongjae Kim <dev@myeongjae.kim>
2019-10-10 20:27:18 +0900

    [#41] Replace MyReflectionUtils with ReflectionTestUtils (#42)

--- content-api/src/test/java/community/content/myanglog/api/dto/PostRequestDtoTest.java
+++ content-api/src/test/java/community/content/myanglog/api/dto/PostRequestDtoTest.java
@@ -3,10 +3,10 @@ package community.content.myanglog.api.dto;
 import static community.content.myanglog.domain.TagTest.getTagFixture;
 import static org.assertj.core.api.BDDAssertions.then;
 
-import community.common.util.MyReflectionUtils;
 import community.content.myanglog.domain.Tag;
 import java.util.Set;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.util.ReflectionTestUtils;
 
 public class PostRequestDtoTest {
   private static Set<Tag> tagFixture = Set.of(getTagFixture());
@@ -14,9 +14,9 @@ public class PostRequestDtoTest {
   public static PostRequestDto getPostRequestDtoFixture() throws Exception {
     PostRequestDto postRequestDto = new PostRequestDto();
 
-    MyReflectionUtils.setField(postRequestDto, "title", "title");
-    MyReflectionUtils.setField(postRequestDto, "content", "content");
-    MyReflectionUtils.setField(postRequestDto, "tags", tagFixture);
+    ReflectionTestUtils.setField(postRequestDto, "title", "title");
+    ReflectionTestUtils.setField(postRequestDto, "content", "content");
+    ReflectionTestUtils.setField(postRequestDto, "tags", tagFixture);
 
     return postRequestDto;
   }

c5cd4dea19c33e093da254f7e3166c7a8dcaf443
Myeongjae Kim <dev@myeongjae.kim>
2019-10-10 20:59:41 +0900

    [#43] notice api의 문서 추가 (#44)
    * [#43] Add documents for notice of mother-api
    
    * [#43] Add missed files

--- content-api/src/main/java/community/content/mjarticle/domain/MjArticleRepository.java
+++ content-api/src/main/java/community/content/mjarticle/domain/MjArticleRepository.java
@@ -4,6 +4,8 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MjArticleRepository extends JpaRepository<MjArticle, Long> {
+
   public Optional<MjArticle> findFirstByIdBeforeOrderByIdDesc(Long id);
+
   public Optional<MjArticle> findFirstByIdAfterOrderByIdAsc(Long id);
 }

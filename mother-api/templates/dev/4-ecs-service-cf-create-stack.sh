db7c0bb9817e26a275d5b8eef43a0d16dec96d7f
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2020-01-19 20:01:43 +0900

    [#develop] Fix script.

--- mother-api/templates/dev/4-ecs-service-cf-create-stack.sh
+++ mother-api/templates/dev/4-ecs-service-cf-create-stack.sh
@@ -4,7 +4,7 @@ python3 4-ecs-service-cf-template.py > 4-ecs-service-cf.template
 
 # services are automatically created by pipeline.
 aws --profile ki cloudformation update-stack \
--stack-name community-mother-api-staging-service \
+--stack-name community-mother-api-dev-service \
 --capabilities CAPABILITY_IAM \
 --template-body file://4-ecs-service-cf.template \
 --parameters \

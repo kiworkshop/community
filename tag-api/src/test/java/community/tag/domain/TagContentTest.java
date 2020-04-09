01f0b4e3c3cabf488b3f58d71bbe881670efb1bc
jiwoo.kim <jiwoo.kim@deliveryhero.co.kr>
2020-04-09 18:42:27 +0900

    [#62] add tagContent test

--- tag-api/src/test/java/community/tag/domain/TagContentTest.java
+++ tag-api/src/test/java/community/tag/domain/TagContentTest.java
@@ -1,12 +1,33 @@
 package community.tag.domain;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static community.tag.domain.TagTest.getTagFixture;
 import static org.assertj.core.api.BDDAssertions.then;
 import static org.assertj.core.api.BDDAssertions.thenThrownBy;
 
-class TagContentTest {
+public class TagContentTest {
+    public static TagContent getTagContentsFixture(Long tagContentId, String tagName) {
+        Tag tag = getTagFixture(tagName);
+        TagContent tagContent = TagContent.builder()
+                    .tag(tag)
+                    .contentType("simplelife")
+                    .contentId(1L).build();
+        ReflectionTestUtils.setField(tagContent, "id", tagContentId);
+        return tagContent;
+    }
+
+    public static List<TagContent> getTagContentsFixture(Tag tag) {
+        return Stream.of(
+                TagContent.builder().tag(tag).contentType("simplelife").contentId(1L).build(),
+                TagContent.builder().tag(tag).contentType("mjarticle").contentId(1L).build()
+        ).collect(Collectors.toList());
+    }
 
     @Test
     void build_ValidInput_ValidOutput() {

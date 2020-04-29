678080726215e1c5b3d92d69aee34da32ca0ff6b
myang <kmh4503@gmail.com>
2020-04-29 20:46:43 +0900

    [#66] Remove disabled annotation

--- comment-api/src/test/java/community/comment/CommentApiApplicationTests.java
+++ comment-api/src/test/java/community/comment/CommentApiApplicationTests.java
@@ -1,13 +1,11 @@
 package community.comment;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CommentApiApplicationTests {
 
-  @Disabled
   @Test
   void contextLoads() {
   }

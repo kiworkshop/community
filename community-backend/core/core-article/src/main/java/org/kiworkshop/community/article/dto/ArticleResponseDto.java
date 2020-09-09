33de2db265dfb9d6b0679f70a4f076b3a9df921b
jaeju.jang <jjj0611@gmail.com>
2020-09-09 15:49:17 +0900

    [#108] article 관련 코어 생성

---
+++ community-backend/core/core-article/src/main/java/org/kiworkshop/community/article/dto/ArticleResponseDto.java
@@ -0,0 +1,31 @@
+package org.kiworkshop.community.article.dto;
+
+import java.time.ZonedDateTime;
+
+import lombok.Builder;
+
+public class ArticleResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+    private String username;
+    private ZonedDateTime createdAt;
+    private ZonedDateTime updatedAt;
+
+    @Builder
+    private ArticleResponseDto(
+        Long id,
+        String title,
+        String content,
+        String username,
+        ZonedDateTime createdAt,
+        ZonedDateTime updatedAt
+    ) {
+        this.id = id;
+        this.title = title;
+        this.content =content;
+        this.username = username;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+}
